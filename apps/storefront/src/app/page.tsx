b06d914da02fd2c56cc633059bc466e8cbfc1112
Vitor Sousa <v.sousa.cf@gmail.com>
2024-04-22 11:38:49 -0300

    feat: :lipstick: add tailwind on storefront application

--- apps/storefront/src/app/page.tsx
+++ apps/storefront/src/app/page.tsx
@@ -5,23 +5,34 @@ export const metadata = {
 	title: "Store | Kitchen Sink",
 };
 
+const buttonVariants = [
+	"default",
+	"destructive",
+	"outline",
+	"secondary",
+	"ghost",
+	"link",
+] as const;
+
 export default function Store(): JSX.Element {
 	log("Hey! This is the Store page.");
 
 	return (
-		<div className="container">
-			<h1 className="title">
-				Store <br />
-				<span>Kitchen Sink</span>
-			</h1>
-			<Button>
-				Button from <code>ui</code> package
-			</Button>
-			<p className="description">
-				Built With <a href="https://turbo.build/repo">Turborepo</a>
-				{" & "}
-				<a href="https://nextjs.org/">Next.js</a>
-			</p>
+		<div className="mt-8 items-center space-y-4">
+			<div className="flex w-full justify-center text-3xl font-bold">
+				Welcome to Next
+			</div>
+			<div className="flex w-full justify-center space-x-4">
+				<ul className="items-center space-y-1 md:inline-flex md:space-x-1 md:space-y-0">
+					{buttonVariants.map((variant) => (
+						<li key={variant}>
+							<Button variant={variant} className="capitalize">
+								{variant}
+							</Button>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
