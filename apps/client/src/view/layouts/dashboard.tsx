cf59dc54c3ce4b8509ef58287a9e396159fadeae
Vitor Sousa <v.sousa.cf@gmail.com>
2024-05-25 20:20:17 -0300

    feat(client): :sparkles: implements dropdown on dashboard layout

--- apps/client/src/view/layouts/dashboard.tsx
+++ apps/client/src/view/layouts/dashboard.tsx
@@ -1,19 +1,119 @@
-import { Button, Icon, ToggleTheme, cn } from "@shared/ui";
+import {
+	Button,
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuPortal,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+	DropdownMenuTrigger,
+	Icon,
+	type IconProps,
+	ToggleTheme,
+	cn,
+} from "@shared/ui";
 import { Outlet } from "react-router-dom";
 
-export function DashboardLayout() {
-	const actived = false;
+const sideItems: { name: string; icon: IconProps["name"] }[] = [
+	{
+		name: "Inbox",
+		icon: "archive",
+	},
+	{
+		name: "Today",
+		icon: "calendar",
+	},
+	{
+		name: "Upcoming",
+		icon: "cardStack",
+	},
+];
 
+function Dropdown() {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="ghost">
+					<Icon name="person" />
+					<span className="sr-only">Open user menu</span>
+					<span className="text-sm ">Jane Doe</span>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-72">
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<div className="flex items-center gap-2">
+							<Icon name="rocket" />
+							Productivity
+						</div>
+						<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<div className="flex items-center gap-2">
+							<Icon name="idCard" />
+							Billing
+						</div>
+						<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<div className="flex items-center gap-2">
+							<Icon name="settings" />
+							Settings
+						</div>
+						<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<div className="flex items-center gap-2">
+							<Icon name="keyboard" />
+							Keyboard shortcuts
+						</div>
+						<DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>Team</DropdownMenuItem>
+					<DropdownMenuSub>
+						<DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
+						<DropdownMenuPortal>
+							<DropdownMenuSubContent>
+								<DropdownMenuItem>Email</DropdownMenuItem>
+								<DropdownMenuItem>Message</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem>More...</DropdownMenuItem>
+							</DropdownMenuSubContent>
+						</DropdownMenuPortal>
+					</DropdownMenuSub>
+					<DropdownMenuItem>
+						New Team
+						<DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>GitHub</DropdownMenuItem>
+				<DropdownMenuItem>Support</DropdownMenuItem>
+				<DropdownMenuItem disabled>API</DropdownMenuItem>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>
+					Log out
+					<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
+
+export function DashboardLayout() {
 	return (
 		<div className="grid min-h-screen w-full lg:grid-cols-[280px_1fr]">
 			<div className="hidden border-r bg-primary-foreground lg:block ">
 				<div className="flex h-full max-h-screen flex-col gap-2">
 					<div className="flex h-[60px] items-center border-b px-6">
-						<a className="flex items-center gap-2 font-semibold" href="/">
-							<Icon name="clipboard" className="h-6 w-6" />
-
-							<span className="">thetodo</span>
-						</a>
+						<Dropdown />
 						<Button className="ml-auto h-8 w-8" size="icon" variant="ghost">
 							<Icon name="bell" className="h-4 w-4" />
 							<span className="sr-only">Toggle notifications</span>
@@ -21,37 +121,27 @@ export function DashboardLayout() {
 					</div>
 					<div className="flex-1 overflow-auto py-2">
 						<nav className="grid items-start px-4 text-sm font-medium">
-							<a
-								className={cn(
-									"flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-accent-foreground",
-									true && "bg-muted text-accent-foreground",
-								)}
-								href="/"
-							>
-								<Icon name="clipboard" className="h-4 w-4" />
-								Todos
-							</a>
-							<a
-								className={cn(
-									"flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-accent-foreground",
-									actived && "bg-muted text-accent-foreground",
-								)}
-								href="/"
-							>
-								<Icon name="calendar" className="h-4 w-4" />
-								Calendar
-							</a>
+							<Button variant={"default"} className="justify-start gap-2">
+								<Icon name="plus" className="h-6 w-6" />
+								Add a task
+							</Button>
+							{sideItems.map((item, index) => {
+								const actived = index === 1;
 
-							<a
-								className={cn(
-									"flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-accent-foreground",
-									actived && "bg-muted text-accent-foreground",
-								)}
-								href="/"
-							>
-								<Icon name="settings" className="h-4 w-4" />
-								Settings
-							</a>
+								return (
+									<a
+										key={item.name}
+										className={cn(
+											"flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-accent-foreground",
+											actived && "bg-muted text-accent-foreground",
+										)}
+										href="/"
+									>
+										<Icon name={item.icon} className="h-4 w-4" />
+										{item.name}
+									</a>
+								);
+							})}
 						</nav>
 					</div>
 				</div>
